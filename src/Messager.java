4052228bd4c925d5f300e3679164afc174e775ff
Maxim Nikolus <nikolusmaxim409@gmail.com>
2024-06-26 17:58:54 +0200

    add Massager

---
+++ src/Messager.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Messager {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Здровствуйте! Что я могу для вас зделать?");
+
+    }
+}
